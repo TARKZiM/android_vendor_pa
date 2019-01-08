c1272e4d1d41f081fe0efeb10cc5a83595fc9613
TARKZiM <tom8476oo@gmail.com>
2019-01-08 11:23:25 +0800

    pa: Add HTC U Ultra (oce)

---
+++ products/oce/pa_oce.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2016-2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_oce,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Advanced platform features
+TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+
+include device/qcom/common/common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/htc/oce/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_oce
+PRODUCT_DEVICE := oce
+PRODUCT_BRAND := htc
+PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := U Ultra
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     BUILD_FINGERPRINT=htc/ocedugl_00400/htc_ocedugl:8.0.0/OPR1.170623.032/1026541.1:user/release-keys"
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+endif
